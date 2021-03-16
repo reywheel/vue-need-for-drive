6c54a67303444436991ba5f5e2081fd2c3a93fa6
Andrey Kolesnikov <mailbox.kolesnikov@mail.ru>
2021-03-16 15:34:10 +0300

    Очистил структуру, подключил sourcemap и создал main view

---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">main</div>
+</template>
+
+<script>
+export default {
+  name: "Main"
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  font-size: 20px;
+}
+</style>
