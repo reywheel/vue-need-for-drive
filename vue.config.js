4a1f68cbbe1a1e2176465e2f5ea1dc1a4ad0e24d
Andrey Kolesnikov <mailbox.kolesnikov@mail.ru>
2021-03-31 21:30:34 +0300

    Настроил обращения на сервер

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,30 @@
-module.exports = {
-  configureWebpack: {
-    devtool: "source-map"
-  },
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/scss/variables.scss";`
+const isDevelopment = process.env.NODE_ENV === "development";
+
+if (isDevelopment) {
+  module.exports = {
+    configureWebpack: {
+      devtool: "source-map"
+    },
+    css: {
+      loaderOptions: {
+        sass: {
+          prependData: `@import "@/scss/variables.scss";`
+        }
+      }
+    },
+    devServer: {
+      proxy: process.env.VUE_APP_API_URL
+    }
+  };
+} else {
+  module.exports = {
+    publicPath: "/vue-need-for-drive/",
+    css: {
+      loaderOptions: {
+        sass: {
+          prependData: `@import "@/scss/variables.scss";`
+        }
       }
     }
-  },
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-need-for-drive/" : "/"
-};
+  };
+}
